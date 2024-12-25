6356bb0b8f98b2e1f743090d6307a1adbb8d63e4
mk43313@gmail.com <mk43313@gmail.com>
2024-12-25 19:12:01 +0900

    refactor: ktlint format 적용(#3)

--- core/navigation/src/main/java/com/example/navigation/MainRoute.kt
+++ core/navigation/src/main/java/com/example/navigation/MainRoute.kt
@@ -7,16 +7,18 @@ sealed class ScreenRouter(val iconId: Int, val title: String)
 
 @Serializable
 data object HomeScreenRoute : ScreenRouter(
-    R.drawable.baseline_home_filled_24, "홈",
+    R.drawable.baseline_home_filled_24,
+    "홈",
 )
 
 @Serializable
 data object MapScreenRoute : ScreenRouter(
-    R.drawable.baseline_map_24, "지도",
+    R.drawable.baseline_map_24,
+    "지도",
 )
 
 @Serializable
-data object MyPageScreenRoute :
-    ScreenRouter(
-        R.drawable.baseline_account_circle_24, "마이 페이지",
-    )
+data object MyPageScreenRoute : ScreenRouter(
+    R.drawable.baseline_account_circle_24,
+    "마이 페이지",
+)
