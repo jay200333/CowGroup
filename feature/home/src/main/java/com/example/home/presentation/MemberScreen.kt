71a18225d7fec2e5099bedfd6c0c37ef38d07fe3
mk43313@gmail.com <mk43313@gmail.com>
2024-12-28 02:57:11 +0900

    feat: MemberScreen 구현(#9)

--- feature/home/src/main/java/com/example/home/presentation/MemberScreen.kt
+++ feature/home/src/main/java/com/example/home/presentation/MemberScreen.kt
@@ -1,20 +1,23 @@
 package com.example.home.presentation
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.home.component.MemberItem
 
 @Composable
 fun MemberScreen(
@@ -39,10 +42,14 @@ fun MemberScreen(
         Column(
             modifier = Modifier
                 .fillMaxSize()
-                .padding(innerPadding),
+                .padding(innerPadding).padding(16.dp),
             horizontalAlignment = Alignment.CenterHorizontally,
         ) {
-            Text(text = "MemberScreen", style = MaterialTheme.typography.displayLarge)
+            LazyColumn(verticalArrangement = Arrangement.spacedBy(8.dp)) {
+                items(count = 10) {
+                    MemberItem("male")
+                }
+            }
         }
     }
 }
