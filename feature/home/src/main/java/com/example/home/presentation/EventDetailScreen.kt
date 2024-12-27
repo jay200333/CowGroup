a0bc8f7a6f501cac722c9c17ad0ed54edb6860f1
mk43313@gmail.com <mk43313@gmail.com>
2024-12-28 01:46:52 +0900

    feat: EventDetailScreen 구현(#7)

--- feature/home/src/main/java/com/example/home/presentation/EventDetailScreen.kt
+++ feature/home/src/main/java/com/example/home/presentation/EventDetailScreen.kt
@@ -1,7 +1,10 @@
 package com.example.home.presentation
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
@@ -19,7 +22,11 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.designsystem.MarkButton
 import com.example.home.R
 
@@ -29,6 +36,8 @@ fun EventDetailScreen(
     onNavigationButtonClick: () -> Unit,
 ) {
     var isBookMarked by remember { mutableStateOf(false) }
+    val totalMember: Int = 10
+    val currentMember: Int = 5
 
     Scaffold(
         modifier = Modifier.fillMaxSize(),
@@ -59,17 +68,28 @@ fun EventDetailScreen(
         Column(
             modifier = Modifier
                 .fillMaxSize()
-                .padding(innerPadding),
-            horizontalAlignment = Alignment.CenterHorizontally,
+                .padding(innerPadding).padding(16.dp),
+            verticalArrangement = Arrangement.spacedBy(8.dp),
         ) {
-            Text(text = "EventDetailScreen", style = MaterialTheme.typography.displaySmall)
+            Text(
+                text = "이벤트 제목",
+                style = MaterialTheme.typography.titleLarge,
+                fontWeight = FontWeight.Bold,
+            )
+            Text(text = "카테고리1", style = MaterialTheme.typography.labelSmall, color = Color.Gray)
+            Text(text = "2024-12-27.01:22", style = MaterialTheme.typography.labelMedium)
             Button(onClick = {}) {
-                Text(text = "지도로 보기")
+                Icon(painter = painterResource(R.drawable.baseline_gps_fixed_24), contentDescription = "btn_event_detail_map")
+                Text(text = "지도로 보기", modifier = Modifier.padding(start = 8.dp))
             }
+            Text(text = "같이 운동하실 분 구합니다.\n같이 프레스 운동하면서 서로 보조해주실 분 구합니다.", style = MaterialTheme.typography.bodyMedium)
+            Text(text = "정원 : ${totalMember}명")
+            Text(text = "참여 인원 : ${currentMember}명")
             Button(onClick = onMemberButtonClick) {
                 Text(text = "전체 참여자 목록 보기")
             }
-            Button(onClick = {}) {
+            Spacer(modifier = Modifier.weight(1f))
+            Button(onClick = {}, modifier = Modifier.fillMaxWidth().align(Alignment.CenterHorizontally)) {
                 Text(text = "참석 하기")
             }
         }
