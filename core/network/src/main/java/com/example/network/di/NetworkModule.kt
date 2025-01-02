55c7c55f29953d08fc7bbab66e14ceea24b8cdf2
mk43313@gmail.com <mk43313@gmail.com>
2025-01-02 23:33:36 +0900

    feat: NetworkModule 생성(#15)

---
+++ core/network/src/main/java/com/example/network/di/NetworkModule.kt
@@ -0,0 +1,29 @@
+package com.example.network.di
+
+import com.example.network.retrofit.CowGroupService
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+
+    @Provides
+    @Singleton
+    fun provideEventApi(): Retrofit {
+        return Retrofit.Builder().baseUrl("https://api.example.com/").addConverterFactory(
+            GsonConverterFactory.create(),
+        ).build()
+    }
+
+    @Provides
+    @Singleton
+    fun eventService(retrofit: Retrofit): CowGroupService {
+        return retrofit.create(CowGroupService::class.java)
+    }
+}
