0d3550c87d355708eed1ef981e6fb68455bfdb2f
mk43313@gmail.com <mk43313@gmail.com>
2025-01-19 17:58:09 +0900

    feat: CreateMeetingScreen 생성(#19)

--- feature/home/src/main/java/com/example/home/presentation/CreateMeetingScreen.kt
+++ feature/home/src/main/java/com/example/home/presentation/CreateMeetingScreen.kt
@@ -1,8 +1,13 @@
 package com.example.home.presentation
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material3.Button
@@ -10,17 +15,23 @@ import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.home.component.CowGroupSlider
+import com.example.home.component.DatePickerTextField
 
 @Composable
 fun CreateMeetingScreen(
     onNavigationButtonClick: () -> Unit,
 ) {
+    val scrollState = rememberScrollState()
+
     Scaffold(
         modifier = Modifier.fillMaxSize(),
         topBar = {
@@ -42,11 +53,85 @@ fun CreateMeetingScreen(
         Column(
             modifier = Modifier
                 .fillMaxSize()
-                .padding(innerPadding),
-            horizontalAlignment = Alignment.CenterHorizontally,
+                .padding(innerPadding)
+                .padding(16.dp),
+            verticalArrangement = Arrangement.spacedBy(4.dp),
         ) {
-            Text(text = "CreateMeetingScreen", style = MaterialTheme.typography.displaySmall)
-            Button(onClick = {}) {
+            Text(
+                text = "모임 이름",
+                style = MaterialTheme.typography.bodyLarge,
+                fontWeight = FontWeight.Bold,
+            )
+            OutlinedTextField(
+                value = "",
+                onValueChange = {},
+                modifier = Modifier.fillMaxWidth(),
+                label = { Text(text = "모임 이름을 입력하세요.") },
+                placeholder = { Text(text = "모임 이름") },
+                singleLine = true,
+            )
+            Text(
+                text = "카테고리 선택",
+                style = MaterialTheme.typography.bodyLarge,
+                fontWeight = FontWeight.Bold,
+            )
+            OutlinedTextField(
+                value = "",
+                onValueChange = {},
+                modifier = Modifier.fillMaxWidth(),
+                label = { Text(text = "카테고리를 입력하세요.") },
+                placeholder = { Text(text = "카테고리") },
+                singleLine = true,
+            )
+            Text(
+                text = "모임 장소",
+                style = MaterialTheme.typography.bodyLarge,
+                fontWeight = FontWeight.Bold,
+            )
+            OutlinedTextField(
+                value = "",
+                onValueChange = {},
+                modifier = Modifier.fillMaxWidth(),
+                label = { Text(text = "모임 장소를 입력하세요.") },
+                placeholder = { Text(text = "모임 장소") },
+                singleLine = true,
+            )
+            Text(
+                text = "모임 날짜",
+                style = MaterialTheme.typography.bodyLarge,
+                fontWeight = FontWeight.Bold,
+            )
+            DatePickerTextField(
+                date = "2023-10-01",
+                onDateSelected = {},
+            )
+            Text(
+                text = "정원",
+                style = MaterialTheme.typography.bodyLarge,
+                fontWeight = FontWeight.Bold,
+            )
+            CowGroupSlider(
+                value = 1f,
+                steps = 100,
+                valueRange = 1f..100f,
+                onValueChange = {},
+            )
+
+            Text(
+                text = "상세 내용",
+                style = MaterialTheme.typography.bodyLarge,
+                fontWeight = FontWeight.Bold,
+            )
+            OutlinedTextField(
+                value = "",
+                onValueChange = {},
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(200.dp).verticalScroll(scrollState),
+                label = { Text(text = "상세 내용을 입력하세요.") },
+                placeholder = { Text(text = "상세 내용") },
+            )
+            Button(onClick = {}, modifier = Modifier.fillMaxWidth().padding(top = 8.dp)) {
                 Text(text = "모임 만들기 버튼")
             }
         }
