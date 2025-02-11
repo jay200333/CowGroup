f0748d567e51ed0ade2f59812a0af23be5194b6b
mk43313@gmail.com <mk43313@gmail.com>
2025-02-11 17:54:07 +0900

    feat: SignUpInfo 모델 생성(#18)

--- core/model/src/main/java/com/example/model/SignUpInfo.kt
+++ core/model/src/main/java/com/example/model/SignUpInfo.kt
@@ -1,7 +1,15 @@
 package com.example.model
 
 data class SignUpInfo(
-    val nickname: String,
+    val username: String,
+    val gender: Gender,
     val email: String,
     val password: String,
 )
+
+enum class Gender(
+    val label: String,
+) {
+    MALE("남성"),
+    FEMALE("여성"),
+}
