98144c48890446b77a5d1c55bd3e2b2d2039d03c
mk43313@gmail.com <mk43313@gmail.com>
2025-02-13 23:10:04 +0900

    feat: Profile 모델 생성(#24)

---
+++ core/model/src/main/java/com/example/model/Profile.kt
@@ -0,0 +1,9 @@
+package com.example.model
+
+data class Profile(
+    val username: String,
+    val introduction: String,
+    val localName: String,
+    val birth: String,
+    val mbti: String
+)
