fef05a39d426daf0c89dc8d28083336ee8df3fc4
mk43313@gmail.com <mk43313@gmail.com>
2025-02-23 21:48:27 +0900

    feat: DetailEvent, Response 모델 생성 (#30)

--- core/model/src/main/java/com/example/model/DetailEvent.kt
+++ core/model/src/main/java/com/example/model/DetailEvent.kt
@@ -1,10 +1,16 @@
 package com.example.model
 
 data class DetailEvent(
+    val id: Int,
     val name: String,
+    val author: String,
     val category: String,
+    val createdDate: String,
+    val address: String,
     val location: String,
+    val content: String,
     val eventDate: String,
     val capacity: Int,
-    val content: String,
+    val applicants: Int,
+    val isBookmarked: Boolean
 )
