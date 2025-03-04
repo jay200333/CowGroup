f18e1d8dfe777a305e5cf87e9491a6148230b8d2
mk43313@gmail.com <mk43313@gmail.com>
2025-03-04 19:50:15 +0900

    refactor: MyPageEventDto bookmarkStatus 필드명 변경(#38)

--- core/network/src/main/java/com/example/network/model/MyPageResponse.kt
+++ core/network/src/main/java/com/example/network/model/MyPageResponse.kt
@@ -3,6 +3,7 @@ package com.example.network.model
 import com.example.model.MyPageEvent
 import com.example.model.MyPageInfo
 import com.example.model.MyPageUserInfo
+import com.google.gson.annotations.SerializedName
 
 data class MyPageResponse(
     val userDto: UserDto,
@@ -24,6 +25,7 @@ data class MyPageEventDto(
     val author: String,
     val name: String,
     val eventDate: String,
+    @SerializedName("bookmarkStatus")
     val status: String,
     val applicants: Int,
     val capacity: Int
