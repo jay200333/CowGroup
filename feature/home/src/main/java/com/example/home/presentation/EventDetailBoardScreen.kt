ab386d40c8483f7489f6cd3fa6cc54f7ae303945
mk43313@gmail.com <mk43313@gmail.com>
2025-04-29 00:25:00 +0900

    feat: EventDetailBoardScreen 생성(#50)

---
+++ feature/home/src/main/java/com/example/home/presentation/EventDetailBoardScreen.kt
@@ -0,0 +1,9 @@
+package com.example.home.presentation
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun EventDetailBoardScreen() {
+    Text(text="게시판 화면")
+}
