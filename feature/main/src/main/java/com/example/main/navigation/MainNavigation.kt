bf185158891692315067f5a4af97f716b08aed15
mk43313@gmail.com <mk43313@gmail.com>
2025-04-28 16:33:10 +0900

    refactor: 모임 생성/편집 후 navigateStack 제거 (#48)

--- feature/main/src/main/java/com/example/main/navigation/MainNavigation.kt
+++ feature/main/src/main/java/com/example/main/navigation/MainNavigation.kt
@@ -6,13 +6,13 @@ import androidx.navigation.NavGraphBuilder
 import com.example.home.navigation.homeNavGraph
 import com.example.home.navigation.navigateCreateMeeting
 import com.example.home.navigation.navigateEventDetail
-import com.example.home.navigation.navigateHome
 import com.example.home.navigation.navigateMember
 import com.example.map.navigation.mapNavGraph
 import com.example.mypage.navigation.myPageNavGraph
 import com.example.mypage.navigation.navigateEditProfile
 import com.example.mypage.navigation.navigateFullMeeting
 import com.example.mypage.navigation.navigateSetting
+import com.example.navigation.HomeScreenRoute
 import com.example.navigation.LoginRoute
 import com.example.navigation.MainGraphRoute
 
@@ -29,7 +29,11 @@ fun NavGraphBuilder.mainNavGraph(
                 popUpTo(MainGraphRoute) { inclusive = true }
             }
         },
-        onHomeScreen = navController::navigateHome,
+        onHomeScreen = {
+            navController.navigate(HomeScreenRoute) {
+                popUpTo(HomeScreenRoute.route) { inclusive = false }
+            }
+        },
         onEventClick = navController::navigateEventDetail,
         onCreateMeetingClick = navController::navigateCreateMeeting,
         onMemberButtonClick = navController::navigateMember,
