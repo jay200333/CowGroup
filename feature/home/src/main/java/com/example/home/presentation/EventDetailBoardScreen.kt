338cb31a4bc33d289d038db6dbe3c997afa29aef
mk43313@gmail.com <mk43313@gmail.com>
2025-05-08 00:59:25 +0900

    feat: EventDetailBoardScreen UI 구현(#50)

--- feature/home/src/main/java/com/example/home/presentation/EventDetailBoardScreen.kt
+++ feature/home/src/main/java/com/example/home/presentation/EventDetailBoardScreen.kt
@@ -1,9 +1,102 @@
 package com.example.home.presentation
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.ExtendedFloatingActionButton
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ModalBottomSheet
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.designsystem.theme.CowGroupTheme
+import com.example.home.R
+import com.example.home.component.BottomSheetContent
+import com.example.home.component.EventDetailBoardItem
 
 @Composable
 fun EventDetailBoardScreen() {
-    Text(text="게시판 화면")
+    val sheetState = rememberModalBottomSheetState()
+    var showBottomSheet by remember { mutableStateOf(false) }
+
+    Scaffold(
+        modifier = Modifier.fillMaxSize(),
+        floatingActionButton = {
+            ExtendedFloatingActionButton(
+                onClick = {},
+                icon = {
+                    Icon(
+                        modifier = Modifier.size(18.dp),
+                        painter = painterResource(R.drawable.baseline_create_24),
+                        contentDescription = "Fab_Event_Detail_HomeScreen",
+                        tint = Color.White
+                    )
+                },
+                text = {
+                    Text(
+                        text = "게시글 등록",
+                        fontSize = 18.sp,
+                        color = Color.White
+                    )
+                },
+                containerColor = MaterialTheme.colorScheme.primary
+            )
+        }
+    ) { innerPadding ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding)
+        ) {
+            LazyColumn(
+                verticalArrangement = Arrangement.spacedBy(12.dp)
+            ) {
+                items(5) {
+                    EventDetailBoardItem(onChatClick = {
+                        showBottomSheet = true
+                    })
+                    HorizontalDivider(
+                        thickness = 5.dp,
+                        color = MaterialTheme.colorScheme.onPrimary
+                    )
+                }
+            }
+        }
+        if (showBottomSheet) {
+            ModalBottomSheet(
+                onDismissRequest = {
+                    showBottomSheet = false
+                },
+                sheetState = sheetState,
+                containerColor = Color.White
+            ) {
+                BottomSheetContent(onAttendButtonClick = {})
+                //val selectedMeeting = meetings.find{ it.id == selectedItemId}
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun EventDetailBoardScreenPreview() {
+    CowGroupTheme {
+        EventDetailBoardScreen()
+    }
 }
