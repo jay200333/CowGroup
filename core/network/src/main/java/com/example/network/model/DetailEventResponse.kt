6c91d8bde9cfe2dfa8c8b0efdbd73c5d2f9bf080
mk43313@gmail.com <mk43313@gmail.com>
2025-05-10 17:52:48 +0900

    refactor: DetailEventResponse 필드 수정 (#52)

--- core/network/src/main/java/com/example/network/model/DetailEventResponse.kt
+++ core/network/src/main/java/com/example/network/model/DetailEventResponse.kt
@@ -1,38 +1,54 @@
 package com.example.network.model
 
 import com.example.model.DetailEvent
+import com.example.model.RegularEvent
 
 data class DetailEventResponse(
     val eventId: Int,
     val name: String,
-    val author: String,
     val category: String,
-    val createdDate: String,
-    val location: String,
     val content: String,
-    val eventDate: String,
     val capacity: Int,
     val applicants: Int,
     val bookmarkStatus: String,
-    val registeredByMe: Boolean,
+    val accessUrl: String?,
+    val eventRegistrant: Boolean,
     val participated: Boolean,
+    val regularEvents: List<RegularEventDto>,
 )
 
-fun DetailEventResponse.toDetailEvent(): DetailEvent {
-    return DetailEvent(
-        id = eventId,
-        name = name,
-        author = author,
-        category = category,
-        createdDate = createdDate,
-        location = location,
-        content = content,
-        eventDate = eventDate,
-        capacity = capacity,
-        applicants = applicants,
-        isBookmarked = bookmarkStatus == "BOOKMARK",
-        editRights = registeredByMe,
-        isParticipated = participated,
+data class RegularEventDto(
+    val id: Int,
+    val name: String,
+    val location: String,
+    val dateTime: String,
+    val capacity: Int,
+    val applicants: Int,
+    val regularRegistrant: Boolean,
+    val participated: Boolean
+)
 
-    )
-}
+fun DetailEventResponse.toDetailEvent(): DetailEvent = DetailEvent(
+    id = eventId,
+    name = name,
+    category = category,
+    content = content,
+    capacity = capacity,
+    applicants = applicants,
+    isBookmarked = bookmarkStatus == "BOOKMARK",
+    url = accessUrl,
+    eventRegistrant = eventRegistrant,
+    isParticipated = participated,
+    regularEvents = regularEvents.map { it.toRegularEvent() }
+)
+
+fun RegularEventDto.toRegularEvent(): RegularEvent = RegularEvent(
+    id = id,
+    name = name,
+    location = location,
+    dateTime = dateTime,
+    capacity = capacity,
+    applicants = applicants,
+    isRegularRegistrant = regularRegistrant,
+    isParticipated = participated
+)
