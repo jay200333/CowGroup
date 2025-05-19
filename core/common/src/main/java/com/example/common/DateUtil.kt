9f9395026ff3ac1e22317e0e0cf042a222cebda4
mk43313@gmail.com <mk43313@gmail.com>
2025-05-19 18:33:08 +0900

    feat: RegularEventResponse 모델 생성 (#56)

--- core/common/src/main/java/com/example/common/DateUtil.kt
+++ core/common/src/main/java/com/example/common/DateUtil.kt
@@ -72,4 +72,12 @@ object DateUtil {
         val outputFormatter = DateTimeFormatter.ofPattern("M/dd (E) a h:mm", Locale.KOREAN)
         return zonedDateTime.format(outputFormatter)
     }
+
+    fun formatIsoToCalendarDate(isoString: String): String {
+        val localDateTime = LocalDateTime.parse(isoString)
+        val seoulZone = ZoneId.of("Asia/Seoul")
+        val zonedDateTime = localDateTime.atZone(ZoneOffset.UTC).withZoneSameInstant(seoulZone)
+        val outputFormatter = DateTimeFormatter.ofPattern("yyyy년 MM월 dd일 (E) a h:mm", Locale.KOREAN)
+        return zonedDateTime.format(outputFormatter)
+    }
 }
