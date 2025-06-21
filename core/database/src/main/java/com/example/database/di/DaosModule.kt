d18c54f02f0b4196ec1b286b845ade4b2fbd2674
mk43313@gmail.com <mk43313@gmail.com>
2025-06-21 18:22:42 +0900

    feat: SearchHistoryDao 의존성 주입 추가 (#66)

--- core/database/src/main/java/com/example/database/di/DaosModule.kt
+++ core/database/src/main/java/com/example/database/di/DaosModule.kt
@@ -1,17 +1,26 @@
 package com.example.database.di
 
 import com.example.database.CowGroupDatabase
+import com.example.database.RegularEventSearchHistoryDatabase
 import com.example.database.dao.SearchHistoryDao
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import javax.inject.Named
 
 @Module
 @InstallIn(SingletonComponent::class)
 internal object DaosModule {
     @Provides
+    @Named("group")
     fun providesSearchHistoryDao(
-        database: CowGroupDatabase,
+        database: CowGroupDatabase
+    ): SearchHistoryDao = database.searchHistoryDao()
+
+    @Provides
+    @Named("regular")
+    fun providesRegularEventSearchHistoryDao(
+        database: RegularEventSearchHistoryDatabase
     ): SearchHistoryDao = database.searchHistoryDao()
 }
