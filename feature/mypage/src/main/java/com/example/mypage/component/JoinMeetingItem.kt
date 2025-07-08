4336d41731e9f971a0ad7cbcaaa0da6518bc2079
User <mk43313@gmail.com>
2025-07-08 23:02:40 +0900

    feat: JoinMeetingScreen UI 구현 (#68)

---
+++ feature/mypage/src/main/java/com/example/mypage/component/JoinMeetingItem.kt
@@ -0,0 +1,112 @@
+package com.example.mypage.component
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.cowgroup.core.designsystem.R
+import com.example.designsystem.component.CowGroupAsyncImage
+import com.example.designsystem.theme.CowGroupTheme
+import com.example.model.Event
+
+@Composable
+fun JoinMeetingItem(event: Event, onEventClick: () -> Unit) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .clickable { onEventClick() },
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        CowGroupAsyncImage(
+            modifier = Modifier
+                .clip(MaterialTheme.shapes.medium)
+                .size(70.dp),
+            imgUrl = event.imageUri,
+            contentDescription = "profile_img",
+        )
+        Spacer(modifier = Modifier.width(12.dp))
+        Column(modifier = Modifier.weight(1f), verticalArrangement = Arrangement.spacedBy(6.dp)) {
+            Text(
+                text = event.name,
+                style = MaterialTheme.typography.titleMedium,
+                fontWeight = FontWeight.SemiBold,
+                color = MaterialTheme.colorScheme.primary
+            )
+            Text(
+                text = event.content,
+                style = MaterialTheme.typography.bodySmall,
+                color = MaterialTheme.colorScheme.onPrimary,
+                maxLines = 1,
+                overflow = TextOverflow.Ellipsis
+            )
+            Row(verticalAlignment = Alignment.CenterVertically) {
+                Icon(
+                    modifier = Modifier
+                        .graphicsLayer { scaleX = -1f }
+                        .size(16.dp),
+                    painter = painterResource(R.drawable.baseline_local_offer_24),
+                    tint = MaterialTheme.colorScheme.onPrimary,
+                    contentDescription = "icon_paging_item_category"
+                )
+                Text(
+                    modifier = Modifier.padding(start = 2.dp),
+                    text = event.category,
+                    style = MaterialTheme.typography.bodySmall,
+                )
+                Spacer(modifier = Modifier.width(8.dp))
+                Icon(
+                    modifier = Modifier
+                        .size(16.dp),
+                    imageVector = Icons.Default.Person,
+                    tint = MaterialTheme.colorScheme.onPrimary,
+                    contentDescription = "icon_paging_item_applicants"
+                )
+                Text(
+                    modifier = Modifier.padding(start = 2.dp),
+                    text = "${event.applicants}명",
+                    style = MaterialTheme.typography.bodySmall,
+                )
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun JoinMeetingItemPreview() {
+    CowGroupTheme {
+        JoinMeetingItem(
+            onEventClick = {},
+            event = Event(
+                id = 1,
+                name = "test1",
+                content = "test1",
+                category = "운동",
+                applicants = 5,
+                capacity = 10,
+                imageUri = "",
+                isBookmarked = false,
+            )
+        )
+    }
+}
--- feature/mypage/src/main/java/com/example/mypage/presentation/JoinMeetingScreen.kt
+++ feature/mypage/src/main/java/com/example/mypage/presentation/JoinMeetingScreen.kt
@@ -1,7 +1,10 @@
 package com.example.mypage.presentation
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material3.Icon
@@ -10,17 +13,42 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.example.model.Event
+import com.example.mypage.component.JoinMeetingItem
+import com.example.mypage.viewmodel.JoinMeetingUIState
+import com.example.mypage.viewmodel.JoinMeetingViewModel
 
 @Composable
 fun JoinMeetingScreen(
-    onNavigationButtonClick: () -> Unit
+    viewModel: JoinMeetingViewModel = hiltViewModel(),
+    onNavigationButtonClick: () -> Unit,
+    onEventClick: (Int) -> Unit
+) {
+    val uiState: JoinMeetingUIState by viewModel.uiState.collectAsState()
+
+    JoinMeetingScreen(
+        onNavigationButtonClick = onNavigationButtonClick,
+        onEventClick = onEventClick,
+        eventList = uiState.eventList
+    )
+}
+
+@Composable
+fun JoinMeetingScreen(
+    onNavigationButtonClick: () -> Unit,
+    onEventClick: (Int) -> Unit,
+    eventList: List<Event>
 ) {
     Scaffold(
         modifier = Modifier.fillMaxSize(),
         topBar = {
             TopAppBar(
-                title = { Text(text = "가입한 모임 목록") },
+                title = { Text(text = "가입한 모임 목록(${eventList.size})") },
                 navigationIcon = {
                     IconButton(onClick = onNavigationButtonClick) {
                         Icon(
@@ -32,6 +60,16 @@ fun JoinMeetingScreen(
             )
         }
     ) { innerPadding ->
-        Text(modifier = Modifier.padding(innerPadding), text = "가입한 모임 목록")
+        Column(modifier = Modifier.padding(innerPadding)) {
+            LazyColumn(
+                modifier = Modifier.padding(horizontal = 16.dp),
+                verticalArrangement = Arrangement.spacedBy(12.dp)
+            ) {
+                items(eventList.size) { index ->
+                    val event = eventList[index]
+                    JoinMeetingItem(event) { onEventClick(event.id) }
+                }
+            }
+        }
     }
 }
