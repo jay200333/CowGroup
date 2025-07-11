a7f7075e47149749d7c91cec73e0024ee7513e67
User <mk43313@gmail.com>
2025-07-11 23:52:26 +0900

    refactor: DateSelector DesignSystem 모듈로 이동 (#68)

--- core/designsystem/src/main/java/com/example/designsystem/component/DateSelector.kt
+++ core/designsystem/src/main/java/com/example/designsystem/component/DateSelector.kt
@@ -1,4 +1,4 @@
-package com.example.home.component
+package com.example.designsystem.component
 
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.Arrangement
