082f81e189b4ed1ebd2e6725c3f992b667fe8482
User <mk43313@gmail.com>
2025-07-24 00:18:07 +0900

    feat: Comment CRUD 함수 ViewModel에 추가 (#68)

--- feature/home/src/main/java/com/example/home/viewmodel/EventDetailBoardViewModel.kt
+++ feature/home/src/main/java/com/example/home/viewmodel/EventDetailBoardViewModel.kt
@@ -1,12 +1,16 @@
 package com.example.home.viewmodel
 
+import androidx.compose.ui.text.TextRange
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import androidx.navigation.toRoute
 import androidx.paging.PagingData
 import androidx.paging.cachedIn
+import com.example.data.repository.CommentRepository
 import com.example.data.repository.PostRepository
+import com.example.model.Comment
 import com.example.model.Post
 import com.example.navigation.EventDetailRoute
 import com.example.network.model.ErrorResponse
@@ -29,9 +33,19 @@ data class EventDetailBoardUIState(
     val message: String = ""
 )
 
+data class EventDetailCommentUIState(
+    val isLoading: Boolean = false,
+    val isEditMode: Boolean = false,
+    val sendButtonEnabled: Boolean = false,
+    val comment: TextFieldValue = TextFieldValue(),
+    val commentList: List<Comment> = emptyList(),
+    val message: String = ""
+)
+
 @HiltViewModel
 class EventDetailBoardViewModel @Inject constructor(
     private val postRepository: PostRepository,
+    private val commentRepository: CommentRepository,
     savedStateHandle: SavedStateHandle
 ) : ViewModel() {
     private val eventId: Int = savedStateHandle.toRoute<EventDetailRoute>().eventId
@@ -40,6 +54,10 @@ class EventDetailBoardViewModel @Inject constructor(
     val uiState: StateFlow<EventDetailBoardUIState> = _uiState.asStateFlow()
     private val pagingPost: Flow<PagingData<Post>> =
         postRepository.getPagingPosts(pageSize = 10, eventId = eventId).cachedIn(viewModelScope)
+
+    private val _commentUiState: MutableStateFlow<EventDetailCommentUIState> =
+        MutableStateFlow(EventDetailCommentUIState())
+    val commentUiState: StateFlow<EventDetailCommentUIState> = _commentUiState.asStateFlow()
 
     init {
         pagingPost.onEach { pagingPost ->
@@ -72,6 +90,169 @@ class EventDetailBoardViewModel @Inject constructor(
                     it.copy(isLoading = false, message = "알 수 없는 오류가 발생했습니다.")
                 }
             }
+        }
+    }
+
+    fun getCommentList(postId: Int) {
+        viewModelScope.launch {
+            _commentUiState.update { state -> state.copy(isLoading = true, message = "") }
+            try {
+                val result = commentRepository.getCommentList(postId)
+                _commentUiState.update { state ->
+                    state.copy(
+                        isLoading = false,
+                        commentList = result
+                    )
+                }
+            } catch (e: HttpException) {
+                val response = e.response()?.errorBody()?.string()
+                val errorResponse = Gson().fromJson(response, ErrorResponse::class.java)
+                _commentUiState.update { state ->
+                    state.copy(
+                        isLoading = false,
+                        message = errorResponse.message
+                    )
+                }
+            } catch (e: Exception) {
+                _commentUiState.update { state ->
+                    state.copy(
+                        isLoading = false,
+                        message = "알 수 없는 오류가 발생했습니다.",
+                    )
+                }
+            }
+        }
+    }
+
+    fun postComment(postId: Int) {
+        viewModelScope.launch {
+            _commentUiState.update { state -> state.copy(isLoading = true, message = "") }
+            try {
+                commentRepository.createComment(postId, _commentUiState.value.comment.text)
+                _commentUiState.update { state ->
+                    state.copy(
+                        isLoading = false,
+                        sendButtonEnabled = false,
+                        comment = TextFieldValue(text = ""),
+                        message = "댓글이 등록되었습니다."
+                    )
+                }
+                getCommentList(postId)
+            } catch (e: HttpException) {
+                val response = e.response()?.errorBody()?.string()
+                val errorResponse = Gson().fromJson(response, ErrorResponse::class.java)
+                _commentUiState.update { state ->
+                    state.copy(
+                        isLoading = false,
+                        message = errorResponse.errors.message
+                    )
+                }
+            } catch (e: Exception) {
+                _commentUiState.update { state ->
+                    state.copy(
+                        isLoading = false,
+                        message = "알 수 없는 오류가 발생했습니다.",
+                    )
+                }
+            }
+        }
+    }
+
+    fun deleteComment(postId: Int?, commentId: Int) {
+        viewModelScope.launch {
+            _commentUiState.update { state -> state.copy(isLoading = true, message = "") }
+            try {
+                commentRepository.deleteComment(commentId)
+                _commentUiState.update { state ->
+                    state.copy(
+                        isLoading = false,
+                        message = "댓글이 삭제되었습니다."
+                    )
+                }
+                getCommentList(postId!!)
+            } catch (e: HttpException) {
+                val response = e.response()?.errorBody()?.string()
+                val errorResponse = Gson().fromJson(response, ErrorResponse::class.java)
+                _commentUiState.update { state ->
+                    state.copy(
+                        isLoading = false,
+                        message = errorResponse.errors.message
+                    )
+                }
+            } catch (e: Exception) {
+                _commentUiState.update { state ->
+                    state.copy(
+                        isLoading = false,
+                        message = "알 수 없는 오류가 발생했습니다.",
+                    )
+                }
+            }
+        }
+    }
+
+    fun editComment(commentId: Int) {
+        viewModelScope.launch {
+            _commentUiState.update { state -> state.copy(isLoading = true, message = "") }
+            try {
+                commentRepository.editComment(commentId, _commentUiState.value.comment.text)
+                _commentUiState.update { state ->
+                    state.copy(
+                        isLoading = false,
+                        isEditMode = false,
+                        sendButtonEnabled = false,
+                        comment = TextFieldValue(text = ""),
+                        message = "댓글이 수정되었습니다."
+                    )
+                }
+            } catch (e: HttpException) {
+                val response = e.response()?.errorBody()?.string()
+                val errorResponse = Gson().fromJson(response, ErrorResponse::class.java)
+                _commentUiState.update { state ->
+                    state.copy(
+                        isLoading = false,
+                        message = errorResponse.errors.message
+                    )
+                }
+            } catch (e: Exception) {
+                _commentUiState.update { state ->
+                    state.copy(
+                        isLoading = false,
+                        message = "알 수 없는 오류가 발생했습니다.",
+                    )
+                }
+            }
+        }
+    }
+
+    fun switchEditMode(commentId: Int) {
+        val editComment = _commentUiState.value.commentList.find { it.id == commentId }
+        val content = editComment?.content.orEmpty()
+        _commentUiState.update { state ->
+            state.copy(
+                isEditMode = true,
+                comment = TextFieldValue(
+                    text = content,
+                    selection = TextRange(content.length)
+                )
+            )
+        }
+    }
+
+    fun cancelEditMode() {
+        _commentUiState.update { state ->
+            state.copy(
+                isEditMode = false,
+                comment = TextFieldValue(text = "")
+            )
+        }
+    }
+
+    fun updateComment(comment: TextFieldValue) {
+        _commentUiState.update { state ->
+            state.copy(
+                comment = comment,
+                sendButtonEnabled = comment.text.isNotBlank()
+            )
         }
     }
 
--- feature/mypage/src/main/java/com/example/mypage/viewmodel/MyPostsViewModel.kt
+++ feature/mypage/src/main/java/com/example/mypage/viewmodel/MyPostsViewModel.kt
@@ -1,14 +1,21 @@
 package com.example.mypage.viewmodel
 
+import androidx.compose.ui.text.TextRange
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.example.data.repository.CommentRepository
+import com.example.model.Comment
 import com.example.model.Post
+import com.example.network.model.ErrorResponse
+import com.google.gson.Gson
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
+import retrofit2.HttpException
 import javax.inject.Inject
 
 data class MyPostsUIState(
@@ -17,10 +24,24 @@ data class MyPostsUIState(
     val postList: List<Post> = emptyList()
 )
 
+data class MyPostsCommentUIState(
+    val isLoading: Boolean = false,
+    val isEditMode: Boolean = false,
+    val sendButtonEnabled: Boolean = false,
+    val comment: TextFieldValue = TextFieldValue(),
+    val commentList: List<Comment> = emptyList(),
+    val message: String = ""
+)
+
 @HiltViewModel
-class MyPostsViewModel @Inject constructor() : ViewModel() {
+class MyPostsViewModel @Inject constructor(private val commentRepository: CommentRepository) :
+    ViewModel() {
     private val _uiState: MutableStateFlow<MyPostsUIState> = MutableStateFlow(MyPostsUIState())
     val uiState: StateFlow<MyPostsUIState> = _uiState.asStateFlow()
+
+    private val _commentUiState: MutableStateFlow<MyPostsCommentUIState> =
+        MutableStateFlow(MyPostsCommentUIState())
+    val commentUiState: StateFlow<MyPostsCommentUIState> = _commentUiState.asStateFlow()
 
     init {
         getPostList()
@@ -55,6 +76,169 @@ class MyPostsViewModel @Inject constructor() : ViewModel() {
                     postList = samplePostList
                 )
             }
+        }
+    }
+
+    fun getCommentList(postId: Int) {
+        viewModelScope.launch {
+            _commentUiState.update { state -> state.copy(isLoading = true, message = "") }
+            try {
+                val result = commentRepository.getCommentList(postId)
+                _commentUiState.update { state ->
+                    state.copy(
+                        isLoading = false,
+                        commentList = result
+                    )
+                }
+            } catch (e: HttpException) {
+                val response = e.response()?.errorBody()?.string()
+                val errorResponse = Gson().fromJson(response, ErrorResponse::class.java)
+                _commentUiState.update { state ->
+                    state.copy(
+                        isLoading = false,
+                        message = errorResponse.message
+                    )
+                }
+            } catch (e: Exception) {
+                _commentUiState.update { state ->
+                    state.copy(
+                        isLoading = false,
+                        message = "알 수 없는 오류가 발생했습니다.",
+                    )
+                }
+            }
+        }
+    }
+
+    fun postComment(postId: Int) {
+        viewModelScope.launch {
+            _commentUiState.update { state -> state.copy(isLoading = true, message = "") }
+            try {
+                commentRepository.createComment(postId, _commentUiState.value.comment.text)
+                _commentUiState.update { state ->
+                    state.copy(
+                        isLoading = false,
+                        sendButtonEnabled = false,
+                        comment = TextFieldValue(text = ""),
+                        message = "댓글이 등록되었습니다."
+                    )
+                }
+                getCommentList(postId)
+            } catch (e: HttpException) {
+                val response = e.response()?.errorBody()?.string()
+                val errorResponse = Gson().fromJson(response, ErrorResponse::class.java)
+                _commentUiState.update { state ->
+                    state.copy(
+                        isLoading = false,
+                        message = errorResponse.errors.message
+                    )
+                }
+            } catch (e: Exception) {
+                _commentUiState.update { state ->
+                    state.copy(
+                        isLoading = false,
+                        message = "알 수 없는 오류가 발생했습니다.",
+                    )
+                }
+            }
+        }
+    }
+
+    fun deleteComment(postId: Int?, commentId: Int) {
+        viewModelScope.launch {
+            _commentUiState.update { state -> state.copy(isLoading = true, message = "") }
+            try {
+                commentRepository.deleteComment(commentId)
+                _commentUiState.update { state ->
+                    state.copy(
+                        isLoading = false,
+                        message = "댓글이 삭제되었습니다."
+                    )
+                }
+                getCommentList(postId!!)
+            } catch (e: HttpException) {
+                val response = e.response()?.errorBody()?.string()
+                val errorResponse = Gson().fromJson(response, ErrorResponse::class.java)
+                _commentUiState.update { state ->
+                    state.copy(
+                        isLoading = false,
+                        message = errorResponse.errors.message
+                    )
+                }
+            } catch (e: Exception) {
+                _commentUiState.update { state ->
+                    state.copy(
+                        isLoading = false,
+                        message = "알 수 없는 오류가 발생했습니다.",
+                    )
+                }
+            }
+        }
+    }
+
+    fun editComment(commentId: Int) {
+        viewModelScope.launch {
+            _commentUiState.update { state -> state.copy(isLoading = true, message = "") }
+            try {
+                commentRepository.editComment(commentId, _commentUiState.value.comment.text)
+                _commentUiState.update { state ->
+                    state.copy(
+                        isLoading = false,
+                        isEditMode = false,
+                        sendButtonEnabled = false,
+                        comment = TextFieldValue(text = ""),
+                        message = "댓글이 수정되었습니다."
+                    )
+                }
+            } catch (e: HttpException) {
+                val response = e.response()?.errorBody()?.string()
+                val errorResponse = Gson().fromJson(response, ErrorResponse::class.java)
+                _commentUiState.update { state ->
+                    state.copy(
+                        isLoading = false,
+                        message = errorResponse.errors.message
+                    )
+                }
+            } catch (e: Exception) {
+                _commentUiState.update { state ->
+                    state.copy(
+                        isLoading = false,
+                        message = "알 수 없는 오류가 발생했습니다.",
+                    )
+                }
+            }
+        }
+    }
+
+    fun switchEditMode(commentId: Int) {
+        val editComment = _commentUiState.value.commentList.find { it.id == commentId }
+        val content = editComment?.content.orEmpty()
+        _commentUiState.update { state ->
+            state.copy(
+                isEditMode = true,
+                comment = TextFieldValue(
+                    text = content,
+                    selection = TextRange(content.length)
+                )
+            )
+        }
+    }
+
+    fun cancelEditMode() {
+        _commentUiState.update { state ->
+            state.copy(
+                isEditMode = false,
+                comment = TextFieldValue(text = "")
+            )
+        }
+    }
+
+    fun updateComment(comment: TextFieldValue) {
+        _commentUiState.update { state ->
+            state.copy(
+                comment = comment,
+                sendButtonEnabled = comment.text.isNotBlank()
+            )
         }
     }
 }
